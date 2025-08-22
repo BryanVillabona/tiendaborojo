db96136dcb5e4e6a8a78b38c148e5a2aa0accc55
BryanVillabona <bvillabona13@gmail.com>
2025-08-22 07:41:05 -0500

    feat: :sparkles: Se añade comentarios de consultas
    By @BryanVillabona

--- consultasarrays.js
+++ consultasarrays.js
@@ -1,15 +1,18 @@
+// 1. Buscar clientes que tengan "natural" en sus preferencias.
 printjson(
     db.clientes.find(
         {preferencias:{$in: ["natural"]}}
     )
 )
 
+// 2. Encontrar productos que tengan al menos los tags "natural" y "orgánico" (usa $all).
 printjson(
     db.productos.find(
         {tags:{$all: ["natural","orgánico"]}}
     )
 )
 
+// 3. Listar productos que tienen más de un tag ($size).
 printjson(
     db.productos.find(
         { $expr: { $gt: [ { $size: "$tags" }, 1 ] } }
--- expresionesregulares.js
+++ expresionesregulares.js
@@ -1,15 +1,18 @@
+// 1. Buscar productos cuyo nombre empiece por "Boro".
 printjson(
     db.productos.find(
         {nombre:{$regex:"^Boro"}}
     )
 )
 
+// 2. Encontrar productos cuyo nombre contenga la palabra "con" (como en “Concentrado de borojó”).
 printjson(
     db.productos.find(
         {nombre:{$regex:"con",$option:"i"}}
     )
 )
 
+// 3. Encontrar clientes cuyo nombre tenga la letra "z" (insensible a mayúsculas/minúsculas).
 printjson(
     db.clientes.find(
         {nombre:{$regex:"[Zz]"}}
